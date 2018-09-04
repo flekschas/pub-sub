b128b5bceffe5b606e6265dd7423019d96a2433b
Fritz Lekschas <service@lekschas.de>
2018-09-03 20:25:47 -0400

    Rename for clarity

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 /**
  * Subscribe to an event.
  * @param {string} event - Event name to subscribe to.
- * @param {function} callback - Function to be called when event of type
+ * @param {function} handler - Function to be called when event of type
  *   `event` is published.
- * @param {number} times - Number of times the callback should called for the
+ * @param {number} times - Number of times the handler should called for the
  *   given event. The event listener will automatically be unsubscribed once
  *   the number of calls exceeds `times`.
- * @return {object} Object with the event name and the callback. The object
+ * @return {object} Object with the event name and the handler. The object
  *   can be used to unsubscribe.
  */
-const subscribe = stack => (event, callback, times = Infinity) => {
+const subscribe = stack => (event, handler, times = Infinity) => {
   if (!stack[event]) {
     stack[event] = [];
     stack.__times__[event] = [];
   }
 
-  stack[event].push(callback);
+  stack[event].push(handler);
   stack.__times__[event].push(parseInt(times, 10) || Infinity);
 
-  return { event, callback };
+  return { event, handler };
 };
 
 /**
@@ -26,18 +26,18 @@ const subscribe = stack => (event, callback, times = Infinity) => {
  *
  * @param {string|object} event - Event from which to unsubscribe or the return
  *   object provided by `subscribe()`.
- * @param {function} callback - Callback function to be unsubscribed. It is
+ * @param {function} handler - Handler function to be unsubscribed. It is
  *   ignored if `id` is provided.
  */
-const unsubscribe = stack => (event, callback) => {
+const unsubscribe = stack => (event, handler) => {
   if (!stack[event]) return;
 
   if (typeof event === 'object') {
     event = event.event; // eslint-disable-line no-param-reassign
-    callback = event.callback; // eslint-disable-line no-param-reassign
+    handler = event.handler; // eslint-disable-line no-param-reassign
   }
 
-  const id = stack[event].indexOf(callback);
+  const id = stack[event].indexOf(handler);
 
   if (!stack[event]) return;
   if (id === -1 || id >= stack[event].length) return;
