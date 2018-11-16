fe2e31286c864c56f8b1e4b23c38d527f7e0c9fa
Fritz Lekschas <service@lekschas.de>
2018-11-16 12:47:07 -0500

    Prettify

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-import { terser } from 'rollup-plugin-terser';
-import buble from 'rollup-plugin-buble';
+import { terser } from "rollup-plugin-terser";
+import buble from "rollup-plugin-buble";
 
 const config = (file, format, plugins) => ({
-  input: 'src/index.js',
+  input: "src/index.js",
   output: {
-    name: 'createPubSub',
-    exports: 'named',
+    name: "createPubSub",
+    exports: "named",
     format,
-    file,
+    file
   },
-  plugins,
+  plugins
 });
 
 export default [
-  config('dist/pub-sub-es.js', 'umd', [buble()]),
-  config('dist/pub-sub-es.min.js', 'umd', [buble(), terser()]),
+  config("dist/pub-sub-es.js", "umd", [buble()]),
+  config("dist/pub-sub-es.min.js", "umd", [buble(), terser()])
 ];
