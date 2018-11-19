a9fde04e667623e6880c988120d723c3729f6b01
Fritz Lekschas <service@lekschas.de>
2018-11-19 14:04:10 -0500

    Test clear() and expand base test

--- tests/index.js
+++ tests/index.js
@@ -27,7 +27,7 @@ globalPubSub.publish(globalEventName);
 globalPubSub.publish(globalEventName);
 
 test("publishes and subscribes to event", t => {
-  t.plan(2);
+  t.plan(3);
 
   const pubSub = createPubSub();
   const eventName = "my-event";
@@ -38,6 +38,11 @@ test("publishes and subscribes to event", t => {
   t.ok(
     pubSub.stack[eventName].length === 1,
     "event stack should have 1 listener"
+  );
+
+  t.ok(
+    pubSub.stack.__times__[eventName][0] === Infinity,
+    "event stack `__times__` should be `Infinity`"
   );
 });
 
@@ -148,6 +153,47 @@ test("allows custom event stack and sets __times__ if necessary", t => {
 
   t.ok(pubSub.stack === myStack, "`myStack` should be the event stack");
   t.ok(pubSub.stack.__times__, "event stack should be the `__times__` prop");
+
+  // A hack to properly close the test runner.
+  globalChannel.close();
+});
+
+test("removes all listeners on clear()", t => {
+  t.plan(6);
+
+  const pubSub = createPubSub();
+
+  let counter = 0;
+
+  const eventName = "my-event";
+  const eventHandler = () => ++counter;
+
+  pubSub.subscribe(eventName, eventHandler);
+
+  pubSub.publish(eventName);
+
+  t.ok(
+    Object.keys(pubSub.stack).length === 2,
+    "stack should have one listener and the `__times__` property"
+  );
+  t.ok(
+    Object.keys(pubSub.stack.__times__).length === 1,
+    "`stack.__times__` should have one entry"
+  );
+  t.ok(counter === 1, "event should have been handled once");
+
+  pubSub.clear();
+  pubSub.publish(eventName);
+
+  t.ok(
+    Object.keys(pubSub.stack).length === 1,
+    "stack should have no listeners and only the `__times__` property"
+  );
+  t.ok(
+    Object.keys(pubSub.stack.__times__).length === 0,
+    "`stack.__times__` should no entries anymore"
+  );
+  t.ok(counter === 1, "new events should not be handled anymore");
 
   // A hack to properly close the test runner.
   globalChannel.close();
